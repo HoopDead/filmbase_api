08622ac429c92d1fcafca66ab2cb7b8be5db66cc
HoopDead <polec0@wp.pl>
2020-05-21 12:01:44 +0200

    New design

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBCol, MDBFormInline } from "mdbreact";
+import { MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText } from "mdbreact";
 import "../index.css";
 
 
@@ -10,15 +10,15 @@ export function DisplayData({data}) {
         {data.results.map(function(result, index) {
             if (result.poster_path) {
               return (
-                    <MDBFormInline key = {index} className = "my-5 data-card w-75 h-50 mx-auto">
-                      <MDBCol xs = "12" md = "4" className = "ml-md-auto mx-sm-auto text-center data-card">
-                        <img src = {`https://image.tmdb.org/t/p/w200/${result.poster_path}`} alt="Plakat filmu"></img>
-                      </MDBCol>
-                      <MDBCol xs = "12" md = "6" className = "mr-md-auto mx-sm-auto text-sm-center text-md-left text-center data-card">
-                      <h3 className = "blue-text data-card">{result.title || result.name || result.original_name }</h3>
-                      <p className = "data-card">{result.overview || "Nie znaleziono opisu w bazie danych."}</p>
-                      </MDBCol>
-                    </MDBFormInline> )
+                    <MDBCol key = {index} xs = "12" md = "8" className = "ml-auto">
+                      <MDBCard className = "flex-md-row mb-4 box-shadow h-md-250">
+                        <MDBCardImage className = "card-img-right flex-auto d-none d-lg-block" src = {`https://image.tmdb.org/t/p/w92/${result.poster_path}`} alt = "Plakat filmu"></MDBCardImage>
+                        <MDBCardBody className = "w-25 w-100 d-flex flex-column align-items-start">
+                          <MDBCardTitle className = "blue-text">{result.title || result.name || result.original_name }</MDBCardTitle>
+                          <MDBCardText className = "text-justify">{result.overview.split(".")[0] || "Nie znaleziono opisu w bazie danych."}</MDBCardText>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol> )
           } else {
             return ( 
               <MDBCol key = {index} md="3" xs = "12" className="my-md-3 my-xs-1 text-center pt-3">
